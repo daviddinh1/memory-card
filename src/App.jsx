e1772cebf527af951be777211b638f18203c1dde
daviddinh1 <91696365+daviddinh1@users.noreply.github.com>
2024-09-01 11:31:03 -0500

    fixed issue where if user clicked same card game wouldnt reset

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import './App.css'
 async function getCats(){
 const response = await fetch('https://api.giphy.com/v1/gifs/search?api_key=bA0ZycF9jl0hWyTJMvyZffbhWb4moeJx&q=cats&limit=6', { mode: 'cors' });
  const catData = await response.json();
- console.log('data:',catData.data);
  return catData.data;
 }
 
--- src/components/Pictures.jsx
+++ src/components/Pictures.jsx
@@ -2,15 +2,42 @@ import {useState} from 'react';
 
 export default function Pictures({ data }) {
  const [currScore, setCurrScore] = useState(0);
- function handleClick(){
-  setCurrScore(currScore + 1);
+ const [indexArr,setIndexArr] = useState([]);
+ function shuffle(array) {
+  let currentIndex = array.length;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+
+    // Pick a remaining element...
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+}
+
+ function handleClick(index){
+  if(indexArr.includes(index)){
+   setCurrScore(0);
+   setIndexArr([]);
+   console.log('index in array called:', index);
+  }
+  else{
+   setCurrScore(currScore + 1);
+   setIndexArr([...indexArr,index]);
+   console.log('indexarr:',indexArr);
+  }
+  shuffle(data);
  }
   return (
      <div>
       <div>{currScore}</div>
       {data.map((gifData, index) => {
-        console.log('Mapping URL:', gifData.images.original.url); // Log each URL
-        return <img src={gifData.images.original.url} alt={`Image ${index}`} key={index} onClick={handleClick} />;
+       // console.log(gifData.id);
+        return <img src={gifData.images.original.url} alt={`Image ${index}`} key={gifData.id} onClick={()=>handleClick(gifData.id)} />;
       })}
     </div>
   );
